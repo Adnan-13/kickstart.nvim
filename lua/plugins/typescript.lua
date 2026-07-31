83ab117a3e6aef5f73f321c12a909c3d8fe9f64a
Adnan Bin Zahir <adnankp09@gmail.com>
2026-07-31 18:48:57 +0600

    feat: add typescript and angular language tooling
    - add typescript-tools.nvim, stop its client in angular projects
    - add angularls root-gated to angular.json/nx.json projects
    - add nvim-ts-autotag and matching treesitter parsers
    - match old prettier formatting args and prettierd config

---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,111 @@
+-- TypeScript/Angular/React stack. Deliberately NOT importing LazyVim's own
+-- `lazyvim.plugins.extras.lang.typescript` extra (which sets up ts_ls/vtsls) -
+-- typescript-tools.nvim replaces that LSP entirely, same as the old config.
+return {
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      local is_angular = lspconfig.util.root_pattern("angular.json", "nx.json")
+
+      require("typescript-tools").setup(vim.tbl_deep_extend("force", opts, {
+        -- Angular projects use angularls (below) to handle TS instead
+        on_attach = function(client, bufnr)
+          if is_angular(vim.api.nvim_buf_get_name(bufnr)) then
+            client.stop()
+          end
+        end,
+        settings = {
+          separate_diagnostic_server = true,
+          publish_diagnostic_on = "insert_leave",
+          expose_as_code_action = "all",
+          complete_function_calls = true,
+          include_completions_with_insert_text = true,
+          disable_member_code_lens = true,
+          tsserver_file_preferences = {
+            includeInlayParameterNameHints = "all",
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
+        },
+      }))
+    end,
+  },
+
+  -- Auto-close/rename HTML/JSX tags
+  { "windwp/nvim-ts-autotag", opts = {} },
+
+  -- Angular's own LSP, only started in actual Angular/Nx projects
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        angularls = {
+          root_dir = function(bufnr, on_dir)
+            local root = require("lspconfig.util").root_pattern("angular.json", "nx.json")(vim.api.nvim_buf_get_name(bufnr))
+            if root then
+              on_dir(root)
+            end
+          end,
+        },
+      },
+    },
+  },
+
+  -- Extra treesitter parsers for the web/Angular stack
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "angular",
+        "jsdoc",
+        "jsonc",
+        "regex",
+        "svelte",
+        "vue",
+        "css",
+        "scss",
+      })
+    end,
+  },
+
+  -- Formatting to match the old prettier setup (4-space, single quotes, etc.)
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        css = { "prettierd", "prettier", stop_after_first = true },
+        angular = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        prettier = {
+          prepend_args = {
+            "--tab-width",
+            "4",
+            "--single-quote",
+            "--jsx-single-quote",
+            "--single-attribute-per-line",
+            "--no-bracket-spacing",
+          },
+        },
+        prettierd = {
+          env = {
+            PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.prettierrc.json"),
+          },
+        },
+      },
+    },
+  },
+}
