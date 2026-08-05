334b6c1661bc00009a2276410b9621778908eb47
Adnan Bin Zahir <adnankp09@gmail.com>
2026-08-05 15:48:47 +0600

    feat: add refactoring.nvim helpers
    editor.refactoring extra: extract func/var, inline var, and debug-print insertion (<leader>r*).

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,6 +40,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     -- Lua-native task runner (<leader>ow/oo/ot)
     { import = "lazyvim.plugins.extras.editor.overseer" },
+    -- Extract func/var, inline var, debug print (<leader>r*)
+    { import = "lazyvim.plugins.extras.editor.refactoring" },
     -- import/override with your plugins
     { import = "plugins" },
   },
